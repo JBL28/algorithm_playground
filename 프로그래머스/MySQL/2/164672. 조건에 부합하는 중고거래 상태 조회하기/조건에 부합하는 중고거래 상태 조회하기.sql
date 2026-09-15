3e0a6a3cfd25921ff4bdf6a33a66eab67d1947ae
JBLee <120627991+JBL28@users.noreply.github.com>
2026-09-16 08:33:50 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/MySQL/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,16 @@
+SELECT
+    BOARD_ID,
+    WRITER_ID,
+    TITLE,
+    PRICE,
+    CASE
+        WHEN STATUS = "DONE" THEN "거래완료"
+        WHEN STATUS = "RESERVED" THEN "예약중"
+        WHEN STATUS = "SALE" THEN "판매중"
+        END as 어쩔티비
+FROM
+    USED_GOODS_BOARD
+WHERE
+    CREATED_DATE like "2022-10-05"
+ORDER BY
+    BOARD_ID DESC
