e0759fab0f85c2c062f17ae56202defb3e493860
JBLee <120627991+JBL28@users.noreply.github.com>
2026-09-19 00:33:15 +0900

    asdf

--- 프로그래머스/Java/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/Java/2/42885. 구명보트/구명보트.java
@@ -2,22 +2,18 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] people, int limit) {
-        ArrayList<Integer> list = new ArrayList<>();
-        for (int a : people)
-            list.add(a);
-        list.sort((Integer a, Integer b) -> Integer.compare(a, b));
-        
         int frontPointer = 0;
         int backPointer = people.length - 1;
         int result = 0;
-        System.out.println(list.toString());
+        
+        Arrays.sort(people);
         
         while (frontPointer <= backPointer) {
             if (frontPointer == backPointer) {
                 result++;
                 frontPointer++;
             }
-            else if (list.get(frontPointer) + list.get(backPointer) <= limit) {
+            else if (people[frontPointer] + people[backPointer] <= limit) {
                 result++;
                 frontPointer++;
                 backPointer--;
