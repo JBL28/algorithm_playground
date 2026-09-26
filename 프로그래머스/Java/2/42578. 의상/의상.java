f0ce0ac15cca599706daac0e6ce508f76651656b
JBLee <120627991+JBL28@users.noreply.github.com>
2026-09-26 11:31:40 +0900

    [level 2] Title: 의상, Time: 0.08 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/Java/2/42578. 의상/의상.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        Map<String, Integer> map = new HashMap<>();
+        
+        for (String[] cloth : clothes) {
+            if (map.containsKey(cloth[1]))
+                map.put(cloth[1], map.get(cloth[1])+1);
+            else
+                map.put(cloth[1], +1);
+        }
+        
+        int result = 1;
+        for(Map.Entry<String, Integer> e : map.entrySet()) {
+            result *= e.getValue() + 1;
+        }
+        
+        return result - 1;
+    }
+}
