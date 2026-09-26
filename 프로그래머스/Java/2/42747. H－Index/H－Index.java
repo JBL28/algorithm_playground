6f8e11cc487aec910c54718fcca952663887a619
JBLee <120627991+JBL28@users.noreply.github.com>
2026-09-26 13:10:18 +0900

    [level 2] Title: H-Index, Time: 6.12 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/Java/2/42747. H－Index/H－Index.java
@@ -0,0 +1,76 @@
+import java.util.*;
+
+class Solution {
+    
+    public int solution(int[] citations) {
+        int LENGTH = citations.length;
+        int result = 0;
+        
+        Arrays.sort(citations);
+        
+        // 더블 이진탐색? -> H-index를 이진탐색하며 탐색하는데, 이 때 인용 수 h 이상인 논문을 이진탐색으로 탐색
+        int MIN = 0;
+        int MAX = citations[LENGTH-1];
+        int MID = (MIN + MAX) / 2;
+        int RESULT = 0;
+        
+        while (MIN < MAX) {
+            int INNER_MIN = 0;
+            int INNER_MAX = LENGTH-1;
+            int INNER_MID = (INNER_MIN + INNER_MAX) / 2;
+            int INNER_RESULT = 0;
+            
+            // MID 이상인 논문 수를 구해야 한다.
+            while(INNER_MIN < INNER_MAX) {
+                if (MID <= citations[INNER_MID])
+                    INNER_MAX = INNER_MID;
+                else 
+                    INNER_MIN = INNER_MID;
+                INNER_MID = (INNER_MIN + INNER_MAX) / 2;
+                
+                if (INNER_MAX - INNER_MIN == 1){
+                    INNER_RESULT = 
+                        (MID <= citations[INNER_MIN]) ? INNER_MIN : INNER_MAX;
+                    break;
+                }
+            }
+            
+            int BIGGER = LENGTH - INNER_RESULT;
+            
+            if (MID <= BIGGER)
+                MIN = MID;
+            else
+                MAX = MID;
+            MID = (MIN + MAX) / 2;
+            
+            if (MAX - MIN == 1){
+                // MAX로 한번 더 innerSearch
+                INNER_MIN = 0;
+                INNER_MAX = LENGTH-1;
+                INNER_MID = (INNER_MIN + INNER_MAX) / 2;
+                INNER_RESULT = 0;
+                while(INNER_MIN < INNER_MAX) {
+                    if (MAX <= citations[INNER_MID])
+                        INNER_MAX = INNER_MID;
+                    else 
+                        INNER_MIN = INNER_MID;
+                    INNER_MID = (INNER_MIN + INNER_MAX) / 2;
+
+                    if (INNER_MAX - INNER_MIN == 1){
+                        INNER_RESULT = 
+                            (MAX <= citations[INNER_MIN]) ? INNER_MIN : INNER_MAX;
+                        break;
+                    }
+                }
+                BIGGER = LENGTH - INNER_RESULT;
+                if (MAX <= BIGGER)
+                    RESULT = MAX;
+                else
+                    RESULT = MIN;
+                break;
+            }
+        }
+        
+        return RESULT;
+    }
+}
